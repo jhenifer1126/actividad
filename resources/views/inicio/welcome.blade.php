eb5a53ba544a52fae39a844331e9cdf1b02e3cc4
jhenifer1126 <e.hnv.jhenifer.plata@cali.edu.co>
2023-06-19 11:33:57 -0500

    ok

--- resources/views/inicio/welcome.blade.php
+++ resources/views/inicio/welcome.blade.php
@@ -23,7 +23,7 @@ https://www.tooplate.com/view/2117-infinite-loop
     <!-- Hero section -->
     <section id="infinite" class="text-white tm-font-big tm-parallax">
         <!-- Navigation -->
-        <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
+        <nav class="navbar navbar-expand-md tm-navbar">
             <div class="container">
                 <div class="tm-next">
                     <a href="#infinite" class="navbar-brand">Estacion Arcoiris Kids</a>
@@ -36,7 +36,6 @@ https://www.tooplate.com/view/2117-infinite-loop
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
-
                         <li class="nav-item">
                             <a class="nav-link tm-nav-link" href="#whatwedo">Quienes somos</a>
                         </li>
@@ -50,27 +49,23 @@ https://www.tooplate.com/view/2117-infinite-loop
                             <a class="nav-link tm-nav-link" href="#contact">Contacto</a>
                         </li>
                     </ul>
-                </div>
-                <div
-                    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                    @if (Route::has('login'))
-                        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                            @auth
-                                <a href="{{ url('/home') }}"
-                                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">hogar</a>
-                            @else
-                                <a href="{{ route('login') }}"
-                                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Iniciar
-                                    seccion</a>
 
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}"
-                                        class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrarse</a>
-                                @endif
-                            @endauth
-                        </div>
-                    @endif
-                </div>
+            </div>
+
+            <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+                @if (Route::has('login'))
+                    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                        @auth
+                            <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        @else
+                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Iniciar seccion</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrarse</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
             </div>
         </nav>
 
